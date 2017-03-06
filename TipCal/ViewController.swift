9812f388eec6446d756eabed5541963163dcea02
Patil, Vijeth <Vijeth_Patil@intuit.com>
2017-03-06 07:56:01 -0800

    Initial Commit

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by Patil, Vijeth on 3/6/17.
+//  Copyright © 2017 mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
