039ada6e3f19336370349a74a3bca31372aba9c9
Patil, Vijeth <Vijeth_Patil@intuit.com>
2017-03-13 02:47:59 -0700

    Initial project commit

---
+++ SettingsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SettingsViewController.swift
+//  TipCal
+//
+//  Created by Patil, Vijeth on 3/13/17.
+//  Copyright © 2017 mobile. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipSlider: UISlider!
+    
+    @IBOutlet weak var defaultTipLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        let defaultTip = (defaults.value(forKey: "defaultTipLabel") ?? 4.00) as! Int
+        defaultTipLabel.text = "\(defaultTip)%"
+        tipSlider.value = (defaults.value(forKey: "defaultTipLabel") ?? 4.00) as! Float
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        var currentValue = Int(tipSlider.value)
+        defaultTipLabel.text = "\(currentValue)%"
+        
+        let defaults = UserDefaults.standard
+        var saveValue = tipSlider.value
+        if(saveValue != 0) {
+            saveValue = saveValue/100;
+        }
+        defaults.set(saveValue, forKey: "defaultTipPercentage")
+        defaults.set(currentValue, forKey:"defaultTipLabel")
+        defaults.synchronize()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+  
+    @IBOutlet weak var defaultTipLabel: UILabel!
+    @IBOutlet weak var tipPercentageSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red:0.07, green:0.75, blue:1.00, alpha:1.0)]
+        
+        self.view.backgroundColor = UIColor.black
+
+        let defaults = UserDefaults.standard
+        let defaultTipPercentageValue = defaults.value(forKey: "defaultTipLabel") as! Int
+        defaultTipLabel.text = "\(defaultTipPercentageValue)%"
+       // let intValue = defaults.integerForKey("defaultTipLabel")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+  
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        let defaultTipPercentageValue = defaults.value(forKey: "defaultTipLabel") as! Int
+        defaultTipLabel.text = "\(defaultTipPercentageValue)%"
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.9, delay: 0.8, options: .curveEaseIn, animations: {
+            self.navigationController?.navigationBar.alpha = 1
+        }, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+ 
+    @IBAction func calculate(_ sender: Any) {
+        calculateTip(sender)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentage = [0.12,0.15,0.18,0.22]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let defaults = UserDefaults.standard
+        let defaultTip = (defaults.value(forKey: "defaultTipPercentage") ?? 0.00) as! Double
+        
+        // let intValue = defaults.integerForKey("defaultTipLabel")
+
+        var tipx = defaultTip
+        if(tipPercentageSegment.selectedSegmentIndex != UISegmentedControlNoSegment) {
+            tipx = tipPercentage[tipPercentageSegment.selectedSegmentIndex]
+        }
+        
+        let tip = bill * tipx
+        
+        
+        let totalAmount = bill + tip
+        tipLabel.text = String(format:"$%.2f",tip)
+        totalLabel.text = String(format:"$%.2f",totalAmount)
+        
+    }
 }
 
